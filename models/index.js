d9020e3f7988c6aa773427e4980c084c4502bf92
nazzim29 <kadouche.n@gmail.com>
2021-09-20 20:51:08 +0100

    normalment ca marche

--- models/index.js
+++ models/index.js
@@ -3,23 +3,32 @@ const path = require("path");
 const Sequelize = require("sequelize");
 
 const db = {};
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
-    dialect: "mysql",
-    host: process.env.DB_URL,
-  });
-console.log('Registring Models ...')
+const sequelize = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USERNAME,
+	process.env.DB_PASSWORD,
+	{
+		dialect: "mysql",
+		dialectModule: require("mysql2"),
+		host: process.env.DB_URL,
+	}
+);
+console.log("Registring Models ...");
 fs.readdirSync(__dirname)
-  .filter((file) => file !== "index.js")
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(sequelize,Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+	.filter((file) => file !== "index.js")
+	.forEach((file) => {
+		const model = require(path.join(__dirname, file))(
+			sequelize,
+			Sequelize.DataTypes
+		);
+		db[model.name] = model;
+	});
 // console.log(db)
 Object.keys(db).forEach(function (modelName) {
-  if ("associate" in db[modelName]) {
-    // console.log(modelName)
-    db[modelName].associate(db);
-  }
+	if ("associate" in db[modelName]) {
+		// console.log(modelName)
+		db[modelName].associate(db);
+	}
 });
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
