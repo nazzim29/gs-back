3d49c54ac005732891e9824041eeef525a44e26a
nazzim29 <kadouche.n@gmail.com>
2021-09-20 20:29:24 +0100

    ddd

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Sequelize = require("sequelize");
 const db = {};
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
     dialect: "mysql",
-    host: "localhost",
+    host: process.env.DB_URL,
   });
 console.log('Registring Models ...')
 fs.readdirSync(__dirname)
