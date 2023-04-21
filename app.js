141a0dc9b05c84161958559275bf11a246c49fa8
nazzim29 <kadouche.n@gmail.com>
2023-04-21 17:10:03 +0100

    f

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
+
 const { sequelize } = require("./models");
 const passport = require("passport");
 const FixObjectAsString = require("./middlewares/FixObjectAsString");
@@ -34,9 +35,14 @@ app.use(bodyParser.text({ extended: true }));
 app.use(morgan("dev"));
 app.use(passport.initialize());
 app.use(FixObjectAsString);
-app.use("/uploads", express.static("./uploads"));
-app.use(cors(corsOptions))
-require("./routes")(app);
+app.use("/uploads", (_, res, next) => {
+    res.set('Cross-Origin-Resource-Policy', 'cross-origin');
+    next();
+  },express.static("./uploads"));
+  const baseRouter = require('express').Router()
+  require("./routes")(baseRouter);
+  app.use(cors(corsOptions),baseRouter)
+
 //* * * * * *
 //chaque jour a  minuit
 console.log(!!parseInt(process.env.PRODUCTION_MODE))
